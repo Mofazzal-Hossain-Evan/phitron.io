f8dc94341778af6cf51a3f67e65e113f4d4a8bfa
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-04-13 16:48:25 +0600

    if else

--- mod2.c
+++ mod2.c
@@ -2,16 +2,30 @@
 #include <stdio.h>
 int main()
 {
+    /*
     int a = 10, b = 3;
     int mod = a % b;
     printf("%d", mod);
 
+*/
+
+int tk;
+    scanf("%d", &tk);
+    if (tk >= 100) 
+    {
+        printf("Burger khabo");
+    } else if (tk >= 50) 
+    {
+        printf("Fuchka khabo");
+    } else if (tk >= 20) 
+    { // Removed the semicolon here
+        printf("Ice cream khabo");
+    } else {
+        printf("kichu e na");
+    }
 
 
 
 
-
-
-    
     return 0;
 }
