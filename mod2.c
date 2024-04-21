26fca415d6d9d28401b6026443602b1831e0063f
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-04-21 17:48:02 +0600

    module3

--- mod2.c
+++ mod2.c
@@ -9,23 +9,47 @@ int main()
 
 */
 
-int tk;
-    scanf("%d", &tk);
-    if (tk >= 100) 
-    {
-        printf("Burger khabo");
-    } else if (tk >= 50) 
-    {
-        printf("Fuchka khabo");
-    } else if (tk >= 20) 
-    { // Removed the semicolon here
-        printf("Ice cream khabo");
-    } else {
-        printf("kichu e na");
-    }
+// int tk;
+//     scanf("%d", &tk);
+//     if (tk >= 100) 
+//     {
+//         printf("Burger khabo");
+//     } else if (tk >= 50) 
+//     {
+//         printf("Fuchka khabo");
+//     } else if (tk >= 20) 
+//     { // Removed the semicolon here
+//         printf("Ice cream khabo");
+//     } else {
+//         printf("kichu e na");
+//     }
+
+
+
+// int tk;
+// scanf("%d", &tk);
+// if(tk >= 5000)       // যদি ৫০০০ টাকা থাকে
+//     {
+//         printf("Cox's Bazar jabo\n");    // তাহলে আমি কক্সবাজার যাব
+//         if(tk >= 10000)     // এরপর আবার চেক করব আমার কাছে ১০০০০ টাকা আছে কিনা
+//         {
+//             printf("Saint Martin jabo\n");   // থাকলে সেইন্ট মারটিন যাব
+//         }
+//         else                 
+//         {
+//             printf("Ferot chole jabo\n");  // সেইন্ট মারটিন যাওয়ার টাকা না থাকলে ফেরত চলে যাব
+//         }
+//     }
+//     else
+//     {
+//         printf("Kothao jabo na\n");  // আর যদি ৫০০০ টাকা ও না থাকে তাহলে কোথাও যাব না।
+//     }
 
 
 
 
-    return 0;
+
+
+
+return 0;
 }
