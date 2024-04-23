5a8543273bde8e65b879e1f18c157f3f3da72a82
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-04-23 23:40:39 +0600

    mod3-7

--- mod3.c
+++ mod3.c
@@ -15,6 +15,34 @@ int main()
 //     printf("%d\n",i);
 // }
 
+// int i;
+// for(i=10; i>=1;i=i-1)
+// {
+//     printf("%d\n", i);
+// }
+
+// int i, n, sum=0;
+// scanf("%d", &n);
+// for(i=1; i<=n; i=i+1) 
+// {
+//     sum += i;
+// }
+// printf("%d", sum);
+
+//মডিউল ৩-6: লুপের সাথে কন্ডিশন
 
 
+int i;
+for(i=1;i<=100;i+=1)
+{
+    if(i%2==0)
+      {
+        printf("%d -Even\n", i);
+      }
+      else
+      {
+        printf("%d -Odd\n" , i);
+      } 
+}
+return 0;
 }
