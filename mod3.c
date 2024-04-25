fe3ffc2955b4dd53d46d28a31baa9a07dd08ef56
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-04-25 14:07:03 +0600

    25april

--- mod3.c
+++ mod3.c
@@ -32,19 +32,41 @@ int main()
 //মডিউল ৩-6: লুপের সাথে কন্ডিশন
 
 
-int i;
-for(i=1;i<=100;i+=1)
-{
-    if(i%2==0)
-      {
-        printf("%d -Even\n", i);
-      }
-      else
-      {
-        printf("%d -Odd\n" , i);
-      } 
-}
+// int i;
+// for(i=1;i<=100;i+=1)
+// {
+//     if(i%2==0)
+//       {
+//         printf("%d -Even\n", i);
+//       }
+//       else
+//       {
+//         printf("%d -Odd\n" , i);
+//       } 
 
+
+
+//  int i,n;
+//  for(i=1;i<=10;i=i+1)
+//  {
+//   if (i==5)
+//   {
+//     break;
+//   }
+//   printf("%d\n",i);
+      
+// }
+
+
+int i,n;
+for(i=1;i<=10;i=i+1)
+  {
+    if (i==5)
+    {
+      continue;
+    }
+    printf("%d\n",i);
+  }
 
 
 
