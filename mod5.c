332568b23e197dc4d444561915cb4945764d8d14
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-05-07 12:10:47 +0600

    mod6

--- mod5.c
+++ mod5.c
@@ -13,24 +13,72 @@ int main() {
 
 //5-5 Capital or Small or Digit
 
-char x;
-scanf("%c", &x);
-if(x>='0' && x<='9')
-{
-printf("IS DIGIT\n");
-}
-else
-{
-    printf("ALPHA\n");
-    if(x>='a' && x<='z')
-    {
-        printf("IS SMALL\n");
-    } 
-    else
-    {
-        printf("IS CAPITAL\n");
-    }
-}
+// char x;
+// scanf("%c", &x);
+// if(x>='0' && x<='9')
+// {
+// printf("IS DIGIT\n");
+// }
+// else
+// {
+//     printf("ALPHA\n");
+//     if(x>='a' && x<='z')
+//     {
+//         printf("IS SMALL\n");
+//     } 
+//     else
+//     {
+//         printf("IS CAPITAL\n");
+//     }
+// }
 
+
+
+// int a,b,c;
+// scanf("%d %d %d", &a,&b,&c);
+// if(a<b && a<c)
+// {
+//     printf("%d", a);
+// }
+// else if(b<a && b<c)
+// {
+//     printf("%d", b);
+// }
+// else 
+// {
+//     printf("%d", c);
+// }
+
+
+// if(a>b && a>c)
+// {
+//     printf("%d", a);
+// }
+// else if(b>a && b>c)
+// {
+//     printf("%d ", b);
+// }
+// else 
+// {
+//     printf("%d", c);
+// }
+
+
+int x = 0;
+    if (x == 1)
+    {
+        if (x >= 0)
+        {
+            printf("true\n");
+        }
+        else
+        {
+            printf("false\n");
+        }
+
+
+    
+
+    }
     return 0;
 }
