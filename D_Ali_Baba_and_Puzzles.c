60117d81b6d6d9ce311af66eb42421ac8d9f60f3
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-05-22 23:33:10 +0600

    arrWithloop

---
+++ D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    long long a,b,c,d;
+
+    
+}
---
+++ mod7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+ int main() {
+//     int arr[3];
+//     arr[0] = 100;
+//     arr[1] = 100;
+//     arr[2] = 100;
+
+//     // Print the address of the array
+//     printf("Address of the array: %p\n", (void *)arr);
+
+//     // Print the elements of the array
+//     for (int i = 0; i < 3; i++) {
+//         printf("arr[%d] = %d\n", i, arr[i]);
+//     }
+
+
+
+// int n;
+// scanf("%d", &n);
+// int ar[n];
+// for(int i = 0; i<n; i++)
+// {
+//     scanf("%d", &ar[i]);
+// }
+// for(int i=n-1;i>=0;i--)
+// {
+//     printf("%d ", ar[i]);
+// }
+
+
+
+
+    return 0;
+}
