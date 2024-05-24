7886b0ce7b627e8e2dd523d294756cd46bc560b6
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-05-24 23:40:38 +0600

    mod7end

--- mod7.c
+++ mod7.c
@@ -30,6 +30,27 @@
 
 
 
+// int n;
+// scanf("%d", &n);
+// int ar[n];
+// for(int i=0; i<n; i++)
+// {
+//     scanf("%d", &ar[i]);
+    
+// }
+// int sum = 0;
+// for(int i=0; i<n;i++)
+// {
+//     sum=sum+ar[i];
+// }
+// printf("%d", sum);
+
+
+// int arrayName[] = {1, 2, 3};
+// printf("%d", arrayName[1]);
+
+int arrayName[] = {1, 2, 3};
+printf("%d", arrayName[3]);
 
     return 0;
 }
