7a6582b7d1ee47839c158e538f7c4339036226a2
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-07-04 21:00:48 +0600

    codeForcePrblm

--- A_Create_A_New_String.c
+++ A_Create_A_New_String.c
@@ -5,5 +5,11 @@
 
 int main() 
 {
-    
+    char s[1001], t[1001];
+    scanf("%s %s", s , t); // Corrected scanf usage
+    int lenS = strlen(s);
+    int lenT = strlen(t);
+    printf("%d %d\n", lenS, lenT); // Corrected Printf to printf
+    printf("%s %s\n", s, t);
+    return 0; // Added return statement for proper main function ending
 }
--- E_Count.c
+++ E_Count.c
@@ -5,5 +5,13 @@
 
 int main() 
 {
-    
+   char a[1000001];
+   scanf("%s",a);
+   int sum=sum+0;
+   for(int i=0; i<strlen(a);i++)
+   {
+    sum = sum+a[i]-'0';
+    //printf("%d\n", a[i]);
+   } 
+   printf("%d",sum);
 }
