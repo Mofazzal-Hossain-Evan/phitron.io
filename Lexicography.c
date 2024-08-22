64c32c1d2bcdd0788d9aac8a5b0118b77ccebe22
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-08-22 18:37:48 +0600

    strcmp

--- Lexicography.c
+++ Lexicography.c
@@ -1,15 +1,57 @@
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//     char a[100], b[100];
+//     scanf("%s %s", a,b);
+//     int i=0;
+//     while(1)
+//     {
+//         if(a[i]=='\0' && b[i]=='\0')
+//         {
+//             printf("same\n");
+//             break;
+//         }
+//         else if(a[i]=='\0')
+//         {
+//             printf("A boro\n");
+//             break;
+//         }
+//         else if(b[i]=='\0')
+//         {
+//             printf("B choto\n");
+//             break;
+//         }
+
+
+
+//         if(a[i]==b[i])
+//         {
+//             i++;
+//         }
+//         else if(a[i]<b[i])
+//         {
+//             printf("a chot\n");
+//             break;;
+//         }
+//         else
+//         {
+//             printf("b choto\n");
+//             break;
+//         }
+
+//     }
+// }
+
+
+
+
 #include<stdio.h>
 #include<string.h>
 int main()
 {
     char a[100], b[100];
-    scanf("%s %s", a.b);
-    int i=0;
-    while(1)
-    {
-        if(a[i]=='\0' && b[i]=='\0')
-        {
-            printf("same");
-        }
-    }
+    scanf("%s %s", a,b);
+    int v=strcmp(a,b);
+    printf("%d",v);
 }
