4bc28cb459fe3bb1cf87e76bac62f1c48483691b
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-08-25 03:09:44 +0600

    final

--- counting_array.c
+++ counting_array.c
@@ -1,3 +1,32 @@
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
+//     int a[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//     int zero=0, one=0;
+//     for(int i=0; i<n; i++)
+//     {
+//         if(a[i]==0)
+//         {
+//             zero++;
+//         }
+//         if(a[i]==1)
+//         {
+//             one++;
+//         }
+//     }
+    
+//     printf("0 - %d \n",zero);
+//     printf("1 - %d \n",one);
+    
+//     }      
+
 #include<stdio.h>
 #include<string.h>
 int main()
@@ -9,11 +38,19 @@ int main()
     {
         scanf("%d",&a[i]);
     }
-    int zero=0;
+    int cnt[7]={0};
     for(int i=0; i<n; i++)
     {
-        if(a[i]==0)
-        {
-            zero++;
-        }
-    }printf("0 - %d \n",zero);}      
+       int val=a[i];
+       cnt[val]++;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d - %d\n",i,cnt[i]);
+    }
+    
+   
+    
+    }      
+
