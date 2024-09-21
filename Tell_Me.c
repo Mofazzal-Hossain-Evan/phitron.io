e9a2cc36ac13d5658b0ebf67984961ce849f2521
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2024-09-22 01:06:27 +0600

    recheck

--- Tell_Me.c
+++ Tell_Me.c
@@ -1,37 +1,39 @@
-#include <stdio.h>
+// #include <stdio.h>
 
 
 
-int main() {
-    int t;
-    scanf("%d", &t);
+// int main() {
+//     int t;
+//     scanf("%d", &t);
 
-    while (t--) {
-        int n;
-        scanf("%d", &n);
+//     while (t--) {
+//         int n;
+//         scanf("%d", &n);
 
-        int arr[100001];
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &arr[i]);
-        }
+//         int arr[100001];
+//         for (int i = 0; i < n; i++) {
+//             scanf("%d", &arr[i]);
+//         }
 
-        int x;
-        scanf("%d", &x);
+//         int x;
+//         scanf("%d", &x);
 
-        int found = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == x) {
-                found = 1;
-                break;
-            }
-        }
+//         int found = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == x) {
+//                 found = 1;
+//                 break;
+//             }
+//         }
 
-        if (found) {
-            printf("YES\n");
-        } else {
-            printf("NO\n");
-        }
-    }
+//         if (found) {
+//             printf("YES\n");
+//         } else {
+//             printf("NO\n");
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+did not understoos
